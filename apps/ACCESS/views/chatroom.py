4455d6c99901fd81785fbdacae9fda8036877cd8
PSGSudalai <sudalaimuthug397@gmail.com>
2024-12-03 09:17:05 +0530

    chat

---
+++ apps/ACCESS/views/chatroom.py
@@ -0,0 +1,11 @@
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from apps.ACCESS.models.chatroom import Message
+
+@api_view(['GET'])
+def get_message_history(request, room_id):
+    messages = Message.objects.filter(chatroom_id=room_id)
+    data = [{'sender': msg.sender.username, 'content': msg.content, 'timestamp': msg.timestamp} for msg in messages]
+    return Response(data)
